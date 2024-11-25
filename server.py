44e636afcfb78b8cc1917100d6475bc8e02c3647
m4rc00000 <marcoacierno09@gmail.com>
2024-11-25 18:09:59 +0100

    bug fix server

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Crea l'applicazione Flask
-app = Flask(__name__)
+app = Flask(__name__, template_folder='app/templates', static_folder='app/public')
+
 
 # Definisci una route per la homepage
 @app.route('/')
+@app.route('/home')
 def home():
-    return "Benvenuto nel server Flask!"
+    return render_template('header.html')
 
 # Avvio del server
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5001)
+    app.run(debug=True)
