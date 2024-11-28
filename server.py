a503428e460051c850a34d9286b1a7de1b7309fa
AldoDamiano <a.damiano26@studenti.unisa.it>
2024-11-28 15:40:04 +0100

    creazione pagina classe studente con relativa visualizzazione

--- server.py
+++ server.py
@@ -10,8 +10,6 @@ app = Flask(__name__, template_folder='app/templates', static_folder="public")  
 app.register_blueprint(classedocente, url_prefix='/')  # Usa '/' o un altro prefisso a tua scelta
 app.register_blueprint(inserimentostudente, url_prefix='/inserimentostudente')  # Il prefisso '/' è opzionale, puoi scegliere uno diverso
 app.register_blueprint(views, url_prefix='/')  # Il prefisso '/' è opzionale, puoi scegliere uno diverso
-
-
 app.register_blueprint(dashboard_bp)
 app.register_blueprint(dashboardDocente_bp)
 # Definisci una route per la homepage
