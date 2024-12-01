0f47cdb5c5dfbef59876f3af80d74c81ac48af86
AugustoCodingRepository <a.persico14@studenti.unisa.it>
2024-12-01 16:30:37 +0100

    Gestione db

--- app/models/scenarioModel.py
+++ app/models/scenarioModel.py
@@ -6,6 +6,6 @@ class ScenarioModel:
         self.db_manager = DatabaseManager()
 
     def aggiungi_scenario(self, scenario_dict):
-        scenario_collection = self.db_manager.get_collection("Scenario")
+        scenario_collection = self.db_manager.get_collection("ScenarioVirtuale")
         scenario_collection.insert_one(scenario_dict)
         print("Scenario aggiunto con successo!")
