163d4f9cefc12ae309edce62ddc0eb086c36cc57
AugustoCodingRepository <a.persico14@studenti.unisa.it>
2024-12-01 17:35:02 +0100

    Gestione db completata, scenario di test inserito con successo, aggiustata la model

--- app/models/scenarioModel.py
+++ app/models/scenarioModel.py
@@ -14,7 +14,7 @@ class ScenarioModel:
         # Get the collection of scenarios
         scenario_collection = self.db_manager.get_collection("ScenarioVirtuale")
 
-        # Query the collection to get the last inserted document, sorted by _id in descending order
+        # Query the collection to get the last inserted scenario, sorted by _id in descending order
         last_scenario = scenario_collection.find_one(sort=[('ID_Scenario', -1)])  # -1 for descending order
 
         # If a scenario exists, return its ID; otherwise, return None
