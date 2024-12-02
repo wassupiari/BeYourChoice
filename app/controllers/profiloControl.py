0bb9973b9cf99c1a2e0003b3dd97d70986984c60
FabrizioCozzolino <f.cozzolino25@studenti.unisa.it>
2024-12-02 21:39:05 +0100

    Commit Gestione password

--- app/controllers/profiloControl.py
+++ app/controllers/profiloControl.py
@@ -1,6 +1,6 @@
 import base64
 import logging
-from flask import flash, jsonify
+from flask import flash, jsonify, session
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -45,59 +45,53 @@ class ProfiloControl:
             logging.error(f"Errore nell'aggiornare il profilo del docente per l'email {email}: {str(e)}")
             return False
 
+    def decode_base64_password(self, stored_password_binary):
+        # Decodifica la password Base64
+        decoded_bytes = base64.b64decode(stored_password_binary)
+        # Converti i bytes decodificati in una stringa, supponendo sia una stringa UTF-8
+        decoded_password = decoded_bytes.decode('utf-8')
+        return decoded_password
 
-
-
-
-def decode_base64_password(stored_password_binary):
-    # Decodifica la password Base64
-    decoded_bytes = base64.b64decode(stored_password_binary)
-    # Converti i bytes decodificati in una stringa, supponendo sia una stringa UTF-8
-    decoded_password = decoded_bytes.decode('utf-8')
-    return decoded_password
-
-
-
-
-def cambiapassword(self, email, old_password, new_password):
-    try:
-        if not email:
-            flash('Email non valida.', 'error')
-            return jsonify({'error': 'Email non valida.'}), 400
-
-        user_email = email.lower()
-        user = self.docente_collection.find_one({'email': user_email})
-
-        if user is None:
-            flash('Utente non trovato.', 'error')
-            return jsonify({'error': 'Utente non trovato.'}), 404
-
-        stored_password_binary = user['password']
-
+    def modifica_password(self, email, old_password, new_password):
         try:
-            stored_password_hash = decode_base64_password(stored_password_binary)
-        except Exception as e:
-            flash(f'Errore durante la decodifica della password: {str(e)}', 'error')
-            return jsonify({'error': f'Errore durante la decodifica della password: {str(e)}'}), 500
+            email = session.get('email')  # Recupero dalla sessione
+            if not email:
+                flash('Email non valida.', 'error')
+                return jsonify({'error': 'Email non valida.'}), 400
 
-        if check_password_hash(stored_password_hash, old_password):
-            new_password_hash = generate_password_hash(new_password)
-            new_password_binary = base64.b64encode(new_password_hash.encode('utf-8'))
+            user_email = email.lower()
+            user = self.docente_collection.find_one({'email': user_email})
 
-            result = self.docente_collection.update_one(
-                {'email': user_email},
-                {'$set': {'password': new_password_binary}}
-            )
-            if result.modified_count > 0:
-                flash('Password cambiata con successo!', 'success')
-                return jsonify({'message': 'Password cambiata con successo!'}), 200
+            if user is None:
+                flash('Utente non trovato.', 'error')
+                return jsonify({'error': 'Utente non trovato.'}), 404
+
+            stored_password_binary = user['password']
+
+            try:
+                stored_password_hash = self.decode_base64_password(stored_password_binary)
+            except Exception as e:
+                flash(f'Errore durante la decodifica della password: {str(e)}', 'error')
+                return jsonify({'error': f'Errore durante la decodifica della password: {str(e)}'}), 500
+
+            if check_password_hash(stored_password_hash, old_password):
+                new_password_hash = generate_password_hash(new_password)
+                new_password_binary = base64.b64encode(new_password_hash.encode('utf-8'))
+
+                result = self.docente_collection.update_one(
+                    {'email': user_email},
+                    {'$set': {'password': new_password_binary}}
+                )
+                if result.modified_count > 0:
+                    flash('Password cambiata con successo!', 'success')
+                    return jsonify({'message': 'Password cambiata con successo!'}), 200
+                else:
+                    flash('Errore nella modifica della password.', 'error')
+                    return jsonify({'error': 'Errore nella modifica della password.'}), 500
             else:
-                flash('Errore nella modifica della password.', 'error')
-                return jsonify({'error': 'Errore nella modifica della password.'}), 500
-        else:
-            flash('Vecchia password errata.', 'error')
-            return jsonify({'error': 'Vecchia password errata.'}), 401
+                flash('Vecchia password errata.', 'error')
+                return jsonify({'error': 'Vecchia password errata.'}), 401
 
-    except Exception as e:
-        flash(f'Errore durante il cambio della password: {str(e)}', 'error')
-        return jsonify({'error': f'Errore durante il cambio della password: {str(e)}'}), 500
+        except Exception as e:
+            flash(f'Errore durante il cambio della password: {str(e)}', 'error')
+            return jsonify({'error': f'Errore durante il cambio della password: {str(e)}'}), 500
