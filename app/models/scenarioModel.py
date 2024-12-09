58523014443476d39e96404414738ba622ac7778
AugustoCodingRepository <a.persico14@studenti.unisa.it>
2024-12-09 22:34:43 +0100

    modificate le variabili

--- app/models/scenarioModel.py
+++ app/models/scenarioModel.py
@@ -7,18 +7,19 @@ class ScenarioModel:
 
     def aggiungi_scenario(self, scenario_dict):
         scenario_collection = self.db_manager.get_collection("ScenarioVirtuale")
+        print(scenario_dict)
         scenario_collection.insert_one(scenario_dict)
         print("Scenario aggiunto con successo!")
 
-    def get_last_scenario_id(self):
+    def get_ultimo_scenario_id(self):
         # Get the collection of scenarios
         scenario_collection = self.db_manager.get_collection("ScenarioVirtuale")
 
         # Query the collection to get the last inserted scenario, sorted by _id in descending order
-        last_scenario = scenario_collection.find_one(sort=[('ID_Scenario', -1)])  # -1 for descending order
+        last_scenario = scenario_collection.find_one(sort=[('id_scenario', -1)])  # -1 for descending order
 
         # If a scenario exists, return its ID; otherwise, return None
         if last_scenario:
-            return int(last_scenario['ID_Scenario'])
+            return int(last_scenario['id_scenario'])
         else:
             return None  # No scenarios found
